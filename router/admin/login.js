94881bea6d656224bed5468962bf74a29e83329e
King <408385413@qq.com>
2019-08-03 15:31:13 +0800

    自学

--- router/admin/login.js
+++ router/admin/login.js
@@ -2,5 +2,8 @@ import { Router } from 'express';
 const router=Router();
 router.get('/',function(req,res,next){
   res.send("登陆页面");
-})
+});
+router.post("/dologin",(req,res)=>{
+  res.send("admin user");
+});
 export default router;
