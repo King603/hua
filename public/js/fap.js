252d901fe71140a0713a4d58e56616519d4580c1
King <408385413@qq.com>
2019-10-24 23:43:01 +0800

    加入Vue

---
+++ public/js/fap.js
@@ -0,0 +1,76 @@
+(() => {
+  new Vue({
+    el: ".common",
+    data: {
+      flower: [
+        ["/aiqingxianhua/", "爱情鲜花"],
+        ["/youqingxianhua/", "友情鲜花"],
+        ["/shengriliwu/", "生日鲜花"],
+        ["/songzhangbeixianhua/", "问候长辈"],
+        ["/zhufuqinghexianhua/", "祝贺鲜花"],
+        ["/hunqingxianhua/", "婚庆鲜花"],
+        ["/tanbingweiwenxianhua/", "探病慰问"],
+        ["/daoqianxianhua/", "道歉鲜花"],
+        ["/businessFlower/kaiyehualan/", "开业花篮"]
+      ],
+      yongshenghua: [
+        ["/yongshenghua/yongshenghua_box.html", "经典花盒"],
+        ["/yongshenghua/yongshenghua_large.html", "巨型玫瑰"],
+        ["/yongshenghua/yongshenghua_lavender.html", "薰衣草"],
+        ["/yongshenghua/yongshenghua_vase.html", "永生瓶花"],
+        ["/yongshenghua/yongshenghua_characteristic.html", "特色永生花"]
+      ],
+      cake: [
+        ["/cake/ganso/", "元祖"],
+        ["/cake/holiland/", "好利来"],
+        ["/cake/croissants/", "可颂坊"],
+        ["/cake/waffleboy/", "窝夫小子"],
+        ["/cake/micamika/", "米卡米卡"],
+        ["/cake/yipinxuan/", "一品轩"],
+        ["/cake/xfxb/", "幸福西饼"],
+        ["/cake/mcake/", "Mcake"],
+        ["/cake/lecake/", "诺心"],
+        ["/cake/vcake/", "Vcake"],
+        ["/cake/allcitycake/", "全国品牌"]
+      ],
+      gifts: [
+        ["/livingflower/", "泰国保鲜花"],
+        ["/gifts/musicbox/", "音乐盒"],
+        ["/gifts/crystallaser/", "水晶内雕"],
+        ["/gifts/goldenflower/", "金箔花"],
+        ["/gifts/cosmeticmirror/", "化妆镜"],
+        ["/gifts/kingking/", "香薰系列"],
+        ["/gifts/bestlife/", "音乐睡枕"],
+        ["/gifts/vase/", "花瓶/相框"],
+        ["/gifts/grose/", "镀金玫瑰"],
+        ["/gifts/candle/", "LED蜡烛"],
+        ["/katonghuashu/", "卡通花束"],
+        ["/toys/", "品牌公仔"],
+        ["/gifts/hifi/", "智能音响"],
+        ["/gifts/shoushi/", "首饰"],
+        ["/gifts/chocolate/", "手工巧克力"],
+        ["/gifts/jinkouqiaokeli/", "进口巧克力"],
+        ["/gifts/dengshi/", "创意礼品"],
+        ["/qiyetuangou/gift_card.html", "礼品卡"]
+      ],
+      Plant: [
+        ["/Plant/greenplant/", "绿色植物"],
+        ["/Plant/potflower/", "盆栽花卉"]
+      ],
+      nav: [
+        ["/", "首页"],
+        ["/flower/", "鲜花"],
+        ["/yongshenghua/", "永生花"],
+        ["/cake/", "蛋糕"],
+        ["/gifts/", "礼品"],
+        ["/huayu/", "花语大全"]
+      ]
+    }
+  })
+})();
+
+
+
+
+
+
